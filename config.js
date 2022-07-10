bfaeac97e74df06012168e08993394d10d8ac50f
shaktiloser <108991610+shaktiloser@users.noreply.github.com>
2022-07-10 14:16:29 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,8 +2,16 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+    
+        apiKey: "AIzaSyApoa5_nMX2-6Qx6fJcARLnd8iojV1gmkU",
+        authDomain: "team-voting-64c51.firebaseapp.com",
+        projectId: "team-voting-64c51",
+        storageBucket: "team-voting-64c51.appspot.com",
+        messagingSenderId: "321891657390",
+        appId: "1:321891657390:web:1ab229b346e737c8132f3f"
+      };
+    
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
